7094e3579877bb4d180924c0df0711f64e29b503
yazin123 <mohammedyazin4@gmail.com>
2025-03-24 12:37:50 +0530

    backend server changed on env, domains on next.cpnfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['project-bucket-001.s3.ap-south-1.amazonaws.com', 'example.com', 'media.linealight.com']
+        domains: ['project-bucket-001.s3.ap-south-1.amazonaws.com', 'example.com', 'media.linealight.com','light-store-demo.s3.ap-south-1.amazonaws.com']
     },
     webpack: (config) => {
         // Add support for importing pdf files and node modules
