bbdebe0dabf8b4376ddc57c82a80f982be29a2ed
yazin123 <mohammedyazin4@gmail.com>
2025-03-24 12:46:02 +0530

    http issue trial 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['project-bucket-001.s3.ap-south-1.amazonaws.com', 'example.com', 'media.linealight.com','light-store-demo.s3.ap-south-1.amazonaws.com']
+        domains: ['project-bucket-001.s3.ap-south-1.amazonaws.com', 'example.com', 'media.linealight.com', 'light-store-demo.s3.ap-south-1.amazonaws.com']
     },
     webpack: (config) => {
         // Add support for importing pdf files and node modules
@@ -15,6 +15,15 @@ const nextConfig = {
     // This helps with certain react-pdf functionality
     experimental: {
         serverActions: true,
+    },
+    // Add rewrites to handle API requests
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://13.235.75.225:3000/api/:path*'
+            }
+        ];
     },
 };
 
