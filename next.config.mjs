e74a0bb55eb776f06cf88dd7e3a42167f0dd23e1
yazin123 <mohammedyazin4@gmail.com>
2025-03-24 23:05:51 +0530

    backend url app.valoled subdomain

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['project-bucket-001.s3.ap-south-1.amazonaws.com', 'example.com', 'media.linealight.com', 'light-store-demo.s3.ap-south-1.amazonaws.com']
+        domains: ['project-bucket-001.s3.ap-south-1.amazonaws.com', 'example.com', 'media.linealight.com','light-store-demo.s3.ap-south-1.amazonaws.com']
     },
     webpack: (config) => {
+        // Add support for importing pdf files and node modules
         config.module.rules.push({
             test: /\.node$/,
             use: 'node-loader',
         });
+
         return config;
     },
+    // This helps with certain react-pdf functionality
     experimental: {
         serverActions: true,
     },
-    async rewrites() {
-        return [
-            {
-                source: '/api/v1/:path*',
-                destination: 'http://13.235.75.225:3000/api/v1/:path*'
-            }
-        ];
-    },
-    // Add a fallback to handle mixed content
-    async headers() {
-        return [
-            {
-                source: '/api/:path*',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: 'upgrade-insecure-requests'
-                    }
-                ]
-            }
-        ];
-    }
 };
 
 export default nextConfig;
